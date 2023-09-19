a9703dc49fd1303708f52ebe417ac0aff800b6aa
Parvinkhuki <khuki.developer@gmail.com>
2023-09-20 02:00:18 +0600

    done

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Post = () => {
     return (
         <div>
-           <NavLink to="/post">shfsff</NavLink> 
+           <NavLink to="/post">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem itaque unde officiis accusantium optio excepturi cupiditate obcaecati ab veritatis voluptatem.</NavLink> 
         </div>
     );
 };
